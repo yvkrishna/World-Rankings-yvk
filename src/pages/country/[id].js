ba6004c398e2c714f12a66eeec9e0e7a84e1932d
yvkrishna <vedhakrishnapanchami@gmail.com>
2020-12-31 21:42:25 +0530

    changed getServerSide to getStatic

--- src/pages/country/[id].js
+++ src/pages/country/[id].js
@@ -20,7 +20,6 @@ const Country = ({country}) => {
         getBorders();
     }, []);
 
-    console.log(borders);
 
     return <Layout title={country.name}>
         <div className={styles.container}>
@@ -105,7 +104,21 @@ const Country = ({country}) => {
 
 export default Country;
 
-export const getServerSideProps = async ({params}) => {
+export const getStaticPaths = async () => {
+    const res = await fetch("https://restcountries.eu/rest/v2/all");
+    const countries = await res.json();
+
+    const paths = countries.map(country => ({
+       params:{id: country.alpha3Code} 
+    }));
+
+    return{
+        paths,
+        fallback: false,
+    }
+}
+
+export const getStaticProps = async ({params}) => {
     const country = await getCountry(params.id)
     return{
         props:{
