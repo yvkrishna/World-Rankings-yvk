22fe5268dce0dae0f4b70646df73c6c57bb1698b
yvkrishna <vedhakrishnapanchami@gmail.com>
2021-01-01 22:01:41 +0530

    credential approach completed

--- src/pages/api/auth/[...nextauth].js
+++ src/pages/api/auth/[...nextauth].js
@@ -1,34 +1,67 @@
 import NextAuth from "next-auth"
 import Providers from "next-auth/providers"
+import { connectToDatabase } from "../../../../utils/mongodb_setup";
+import bcrypt from 'bcrypt'
+
+
+const rounds = 10
 
 const options = {
     providers:[
-        Providers.GitHub({
-            clientId: process.env.GITHUB_ID,
-            clientSecret: process.env.GITHUB_SECRET
-        }),
-        Providers.Auth0({
-            clientId: process.env.AUTH0_CLIENT_ID,
-            clientSecret: process.env.AUTH0_CLIENT_SECRET,
-            domain: process.env.AUTH0_DOMAIN
-        }),
-        Providers.Email({
-            server:{
-                host:process.env.EMAIL_SERVER_HOST,
-                port:process.env.EMAIL_SERVER_PORT,
-                auth:{
-                    user:process.env.EMAIL_SERVER_USER,
-                    pass:process.env.EMAIL_SERVER_PASSWORD,
-                },
+        Providers.Credentials({
+            // The name to display on the sign in form (e.g. 'Sign in with...')
+            name: 'Credentials',
+            // The credentials is used to generate a suitable form on the sign in page.
+            // You can specify whatever fields you are expecting to be submitted.
+            // e.g. domain, username, password, 2FA token, etc.
+            credentials: {
+              username: { label: "Username", type: "text", placeholder: "Enter Username" },
+              password: {  label: "Password", type: "password", placeholder: "Enter Password" }
             },
-            from: process.env.EMAIL_FROM,        
-        }),
+            authorize: async (credentials) => {
+              // Add logic here to look up the user from the credentials supplied
+              const user_details = { username: credentials.username, password: credentials.password }
+                
+              const verifyLogin = async (unp_details) => {
+                const { db } = await connectToDatabase();
+                var {username, password} = unp_details;
+                const member = await db
+                    .collection("members")
+                    .findOne({username:username})
+                var isAuth = await bcrypt.compare(password, member.password);
+
+                  if(isAuth){
+                      return member;
+                  }else{
+                      return null;
+                  }
+              }
+
+              var user = await verifyLogin(user_details) 
+
+              if (Object.values(user).length>0){
+                // Any object returned will be saved in `user` property of the JWT
+                // console.log("============= Credentials in database ===========")
+                // console.log(user)
+                // console.log("============= Credentials in database ===========")
+                return Promise.resolve(user)
+              }else{
+                // If you return null or false then the credentials will be rejected
+                return Promise.resolve(null); 
+                // You can also Reject this callback with an Error or with a URL:
+                // return Promise.reject(new Error('error message')) // Redirect to error page
+                // return Promise.reject('/path/to/redirect')        // Redirect to a URL
+              }
+            }
+          })
     ],
-    database: {
-        type:"mongodb",
-        database:":memory:",
-        synchronize:true,
-    }
+    session: {
+      jwt: true, 
+    },
+    pages: {
+        signIn: '/authenticate/LogIn',
+        signOut: '/authenticate/LogIn',
+    },
 }
 
 export default (req,res) => NextAuth(req,res,options);
--- src/pages/authenticate/LogIn.js
+++ src/pages/authenticate/LogIn.js
@@ -1,14 +1,15 @@
 import styles from "./LogIn.module.css"
 import Link from "next/link"
 import Image from 'next/image'
+import { providers, signIn, csrfToken } from 'next-auth/client'
 
-export default function Login() {
+export default function Login({ providers, csrfToken }) {
   return (
     <div className={styles.Login_App}>
         <div className="container shadow-lg p-3 mb-5 rounded" className={styles.main_part}>
 		<div className="row d-flex justify-content-center">
 			<div className="col-1 col-md-1 col-sm-1"></div>	
-			<div className="col-10 col-md-10 col-sm-10" style={{"width":"100%;"}}>
+			<div className="col-10 col-md-10 col-sm-10" style={{"width":"100%"}}>
 					<div className="d-flex justify-content-center">
 						<Image src="/profile.svg" alt="avatar" className={styles.img2}  width="100" height="100"/>
 					</div>
@@ -17,26 +18,35 @@ export default function Login() {
 						<h1 className="p-1 bd-highlight">WELCOME</h1>
 					</div>
 					<div className="d-flex justify-content-center">
-						<strong className="p-2 bd-highlight" style={{"margin-top": "-15px;"}}>Add your details</strong>
+						<strong className="p-2 bd-highlight" style={{"marginTop": "-15px"}}>Add your details</strong>
 					</div>
 					<br />
 					<div>
-                        <form action="/login-done" method="POST" name="myform">
+                        <form name="myform" method='post' action='/api/auth/callback/credentials'>
+						<input name='csrfToken' type='hidden' defaultValue={csrfToken}/>
 							<div className="form-group">
 								<input type="text" className="form-control" name="username" aria-describedby="emailHelp" placeholder="Enter Id" />
 							</div>
 							  <div className="form-group">
 								<input type="password" className="form-control" id="password" name="password" placeholder="Password" />
 							  </div>	
-							  <button type="submit" className="btn btn-primary" value="submit" onclick="validate()">Login</button>
+							  <button type="submit" className="btn btn-primary" value="submit">Login</button>
 							<br /><br />
 						  <small>Don't have an Account <Link href="/authenticate/Register">Signup</Link></small>
 						</form>
 					</div>
 			</div>
-			<div class="col-1 col-md-1 col-sm-1"></div>
+			<div className="col-1 col-md-1 col-sm-1"></div>
 		</div>
 	</div>
     </div>
   );
 }
+
+
+Login.getInitialProps = async (context) => {
+	return {
+	  providers: await providers(context),
+	  csrfToken: await csrfToken(context)
+	}
+  }
